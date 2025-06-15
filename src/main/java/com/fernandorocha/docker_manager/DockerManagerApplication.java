22f2e46952a6ea1a863fb46b6f3afb7849cd332f
Fernando Rocha <beneditofernandor@gmail.com>
2025-06-15 16:31:51 -0300

    initial commit

---
+++ src/main/java/com/fernandorocha/docker_manager/DockerManagerApplication.java
@@ -0,0 +1,14 @@
+package com.fernandorocha.docker_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerManagerApplication.class, args);
+	}
+
+}
+
