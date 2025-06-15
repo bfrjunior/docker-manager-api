85a9a60d932bd0c5f8740203cb201cc7d9e6b829
Fernando Rocha <beneditofernandor@gmail.com>
2025-06-15 17:11:55 -0300

    feat: controller

---
+++ src/main/java/com/fernandorocha/docker_manager/controller/DockerContainersController.java
@@ -0,0 +1,9 @@
+package com.fernandorocha.docker_manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/containers")
+public class DockerContainersController {
+}
